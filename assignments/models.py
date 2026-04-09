0ad423f612b34482afda285bb6e839f542debaf6
avaleajay170 <avaleajay170@gmail.com>
2026-04-09 08:44:57 +0530

    feat: student upload sample, submit assignment, view and re-upload

--- assignments/models.py
+++ assignments/models.py
@@ -23,7 +23,7 @@ class Submission(models.Model):
     ]
     assignment     = models.ForeignKey(Assignment, on_delete=models.CASCADE, related_name='submissions')
     student        = models.ForeignKey(StudentProfile, on_delete=models.CASCADE, related_name='submissions')
-    image          = models.ImageField(upload_to='submissions/')
+    image          = models.FileField(upload_to='submissions/')
     status         = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
     teacher_remark = models.TextField(blank=True)
     submitted_at   = models.DateTimeField(auto_now_add=True)
